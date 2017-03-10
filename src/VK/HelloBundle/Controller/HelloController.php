31d641820e522ff5a5126dc38ce8dceff157f335
volodymyr-vk <volodymyr.v.kondratenko@gmail.com>
2017-03-11 00:05:21 +0200

    Rename DefaultController to  HelloController

--- src/VK/HelloBundle/Controller/HelloController.php
+++ src/VK/HelloBundle/Controller/HelloController.php
@@ -4,10 +4,10 @@ namespace VK\HelloBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class HelloController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('VKHelloBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('VKHelloBundle:Hello:index.html.twig', array('name' => $name));
     }
 }
